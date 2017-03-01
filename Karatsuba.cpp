b7eddfd61b67db5d36b73e9c8fcdc7546ab6d1d8
Karan Siwach <karansiwach360@gmail.com>
2017-03-01 23:13:50 +0530

    Update Karatsuba.cpp

--- Karatsuba.cpp
+++ Karatsuba.cpp
@@ -11,48 +11,8 @@ using namespace std;
 #define se                  second
 #define pii                 pair<ll,ll>
 #define all(c)              c.begin(),c.end()
-#define mini(c)             min_element(all(c))
-#define maxi(c)             max_element(all(c))
-ll gcd(ll a, ll b)
-{
-    return b == 0 ? a : gcd(b, a % b);
-}
-ll lcm(ll a, ll b)
-{
-    return a * (b / gcd(a, b));
-}
-ll Max(ll a, ll b)
-{
-    return a > b ? a : b;
-}
-ll Min(ll a, ll b)
-{
-    return a < b ? a : b;
-}
-ll power(ll x, ll y,ll p)
-{
-    ll res=1;
-    x=x%p;
-    while(y>0)
-    {
-        if(y&1)
-            res=((res%p)*(x%p))%p;
-        y=y>>1;
-        x=((x%p)*(x%p))%p;
-    }
-    return res;
-}
-bool isprime(ll n)
-{
-    ll z=sqrt(n);
-    if(n==2) return true;
-    if(n%2==0) return false;
-    for(ll i=3; i<=z; i+=2)
-    {
-        if(n%i==0) return false;
-    }
-    return true;
-}
+ll Max(ll a, ll b) return a > b ? a : b;
+ll Min(ll a, ll b) return a < b ? a : b;
 string addstrings(string s1,string s2)
 {
     reverse(all(s1));
