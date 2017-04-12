22b58cf76865602e8c7bf7b4566043b699b31565
Karan Siwach <karansiwach360@gmail.com>
2017-04-12 23:21:02 +0530

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -26,16 +26,30 @@ int main()
     rep(i,n) cin>>value[i];
     rep(i,n) cin>>weight[i];
     ll dp[n+1][w+1];
-    rep(j,w+1) dp[0][j]=0;
-    rep(i,n+1) dp[i][0]=0;
+    ll sel[n+1][w+1];
+    rep(j,w+1) dp[0][j]=0,sel[0][j]=-1;
+    rep(i,n+1) dp[i][0]=0,sel[i][0]=-1;
     rep(i,n)
     {
         rep(j,w)
         {
             dp[i+1][j+1]=dp[i][j+1];
-            if(j+1-weight[i]>=0) dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j+1-weight[i]]+value[i]);
+            sel[i+1][j+1]=sel[i][j+1];
+            if(j+1-weight[i]>=0)
+            {
+                if(dp[i][j+1-weight[i]]+value[i]>dp[i+1][j+1]) sel[i+1][j+1]=i+1;
+                dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j+1-weight[i]]+value[i]);
+            }
         }
     }
-    cout<<dp[n][w];
+    cout<<dp[n][w]<<endl;
+    ll r=n,c=w;
+    while(r!=-1 && sel[r][c]!=-1)
+    {
+        cout<<sel[r][c]<<": ("<<weight[sel[r][c]-1]<<","<<value[sel[r][c]-1]<<")"<<endl;
+        ll R=r,C=c;
+        c-=weight[sel[R][C]-1];
+        r=sel[R][C]-1;
+    }
     return 0;
 }
