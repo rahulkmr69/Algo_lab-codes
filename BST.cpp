16f77537e5fbdc2eb7eddc52513587c1111cc121
rahulkmr69 <93141622+rahulkmr69@users.noreply.github.com>
2021-10-25 21:26:38 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -241,7 +241,7 @@ int main()
 {
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
-    ios_base::sync_with_stdio(false);
+//     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     ll q;
